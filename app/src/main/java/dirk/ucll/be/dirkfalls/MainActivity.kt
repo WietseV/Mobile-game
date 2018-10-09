6cb468e08e7b2324dda6eaf8fef9b79440e0067a
Robin Van den Broeck <robin.broeck@gmail.com>
2018-10-09 12:16:01 +0200

    Added game loop

--- app/src/main/java/dirk/ucll/be/dirkfalls/MainActivity.kt
+++ app/src/main/java/dirk/ucll/be/dirkfalls/MainActivity.kt
@@ -3,6 +3,8 @@ package dirk.ucll.be.dirkfalls
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import java.util.*
+import kotlin.concurrent.scheduleAtFixedRate
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,5 +13,14 @@ class MainActivity : AppCompatActivity() {
 
         val textView = findViewById<TextView>(R.id.hello_text)
         textView.text = "Hello dirk"
-    }
+
+        val board = Board(1f, 1f)
+        val fps = 30L
+        val msInSecond = 1000L
+        val timeBetweenFrames = msInSecond / fps
+        val gameLoop = Timer("Game loop", true)
+        gameLoop.scheduleAtFixedRate(0, timeBetweenFrames) {
+            board.updateEntities()
+        }
+     }
 }

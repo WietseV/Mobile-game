a34594d89c5b67956cd86640e284412a6f7ce41d
Robin Van den Broeck <robin.broeck@gmail.com>
2018-10-09 11:48:30 +0200

    Added base interfaces

--- app/src/main/java/dirk/ucll/be/dirkfalls/MainActivity.kt
+++ app/src/main/java/dirk/ucll/be/dirkfalls/MainActivity.kt
@@ -2,11 +2,14 @@ package dirk.ucll.be.dirkfalls
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.hello_text)
+        textView.text = "Hello dirk"
     }
 }
---
+++ app/src/main/java/dirk/ucll/be/dirkfalls/Vector2.kt
@@ -0,0 +1,3 @@
+package dirk.ucll.be.dirkfalls
+
+data class Vector2(var x: Float, var y: Float)

7f7047a20fe89b4961f913dc2e0fd4c843da854a
JaroDeklerck <jaro.deklerck@student.kuleuven.be>
2018-10-16 11:50:31 +0200

    Commet is Comet

--- app/src/main/java/dirk/ucll/be/dirkfalls/entities/Comet.kt
+++ app/src/main/java/dirk/ucll/be/dirkfalls/entities/Comet.kt
@@ -2,7 +2,7 @@ package dirk.ucll.be.dirkfalls.entities
 
 import dirk.ucll.be.dirkfalls.Vector2
 
-class Commet(override var position: Vector2, override var velocity: Vector2) : Entity {
+class Comet(override var position: Vector2, override var velocity: Vector2) : Entity {
     override fun init() {
     }
 

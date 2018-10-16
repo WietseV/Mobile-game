1e63dfea1a869048af604e4ffa98111c0a70d995
Gleb S <gleb.shalabotov@student.ucll.be>
2018-10-16 11:12:19 +0200

    fucked up

--- app/src/main/java/dirk/ucll/be/dirkfalls/Board.kt
+++ app/src/main/java/dirk/ucll/be/dirkfalls/Board.kt
@@ -5,6 +5,7 @@ import dirk.ucll.be.dirkfalls.entities.Entity
 class Board(val width: Float, val height: Float) {
     private val entities = mutableListOf<Entity>()
 
+
     fun updateEntities() {
         entities.forEach { it.update() }
     }

d39940ef3e4ce38ee727d619177b62273008bbc0
Robin Van den Broeck <robin.broeck@gmail.com>
2018-10-16 12:02:20 +0200

    Added methods to vector class

--- app/src/main/java/dirk/ucll/be/dirkfalls/Vector2.kt
+++ app/src/main/java/dirk/ucll/be/dirkfalls/Vector2.kt
@@ -1,3 +1,35 @@
 package dirk.ucll.be.dirkfalls
 
-data class Vector2(var x: Float, var y: Float)
+data class Vector2(val x: Float, val y: Float) {
+    operator fun plus(other: Vector2): Vector2 {
+        return Vector2(x + other.x, y + other.y)
+    }
+
+    operator fun plus(scalar: Float): Vector2 {
+        return plus(Vector2(scalar, scalar))
+    }
+
+    operator fun minus(other: Vector2): Vector2 {
+        return Vector2(x - other.y, y + other.y)
+    }
+
+    operator fun minus(scalar: Float): Vector2 {
+        return minus(Vector2(scalar, scalar))
+    }
+
+    operator fun times(other: Vector2): Vector2 {
+        return Vector2(x * other.x, y * other.y)
+    }
+
+    operator fun times(scalar: Float): Vector2 {
+        return times(Vector2(scalar, scalar))
+    }
+
+    operator fun div(other: Vector2): Vector2 {
+        return Vector2(x / other.x, y / other.y)
+    }
+
+    operator fun div(scalar: Float): Vector2 {
+        return div(Vector2(scalar, scalar))
+    }
+}

c7381678024dff5704381a86d0523834e5a5b933
Gleb S <gleb.shalabotov@student.ucll.be>
2018-11-02 11:33:30 +0100

    Ik heb een sample voor jullie hier in toegevoegd. je kan het runnen en dan G, C, R of P drukken om te tekenen

--- desktop/src/be/ucll/dirkfalls/desktop/DesktopLauncher.java
+++ desktop/src/be/ucll/dirkfalls/desktop/DesktopLauncher.java
@@ -2,11 +2,17 @@ package be.ucll.dirkfalls.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
 import be.ucll.dirkfalls.DirkFallsGame;
+import be.ucll.dirkfalls.samples.ShapeRenderSample;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new DirkFallsGame(), config);
-	}
+    public static void main(String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.width = 1080;
+        config.height = 720;
+
+        //new LwjglApplication(new DirkFallsGame(), config);
+        new LwjglApplication(new ShapeRenderSample(), config);
+    }
 }

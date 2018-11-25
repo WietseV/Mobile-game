c9591f72c25855d5ffe0f49e66f4d80309284cf8
Robin Van den Broeck <robin.broeck@gmail.com>
2018-11-25 16:30:05 +0100

    Reworked GameTouchAdapter to use the current hero position

--- core/src/be/ucll/dirkfalls/screen/GameTouchAdapter.kt
+++ core/src/be/ucll/dirkfalls/screen/GameTouchAdapter.kt
@@ -1,24 +1,43 @@
 package be.ucll.dirkfalls.screen
 
-import be.ucll.dirkfalls.GameConfig
+import be.ucll.dirkfalls.GameConfig.WORLD_WIDTH
 import be.ucll.dirkfalls.entities.Hero
-import be.ucll.dirkfalls.entities.HeroDirection
+import be.ucll.dirkfalls.entities.HeroDirection.*
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.InputAdapter
+import kotlin.math.roundToInt
 
 internal class GameTouchAdapter(private val hero: Hero) : InputAdapter() {
     override fun touchDown(screenX: Int, screenY: Int, pointer: Int, button: Int): Boolean {
-        val x = screenX * 1f / Gdx.graphics.width
-        hero.direction = when {
-            x * GameConfig.WORLD_WIDTH < GameConfig.WORLD_WIDTH / 2 -> HeroDirection.LEFT
-            x * GameConfig.WORLD_WIDTH > GameConfig.WORLD_WIDTH / 2 -> HeroDirection.RIGHT
-            else -> HeroDirection.STILL
-        }
-        return super.touchDown(screenX, screenY, pointer, button)
+        switchHeroDirection(screenX)
+        return true
+    }
+
+    override fun touchDragged(screenX: Int, screenY: Int, pointer: Int): Boolean {
+        switchHeroDirection(screenX)
+        return true
     }
 
     override fun touchUp(screenX: Int, screenY: Int, pointer: Int, button: Int): Boolean {
-        hero.direction = HeroDirection.STILL
-        return super.touchUp(screenX, screenY, pointer, button)
+        hero.direction = STILL
+        return true
+    }
+
+    /**
+     * @param screenX Where the current press is
+     */
+    private fun switchHeroDirection(screenX: Int) {
+        val heroX = (hero.shape.x * (Gdx.graphics.width / WORLD_WIDTH)).roundToInt()
+
+        hero.direction = when {
+            between(screenX, heroX - 50, heroX + 50) -> STILL
+            screenX < heroX -> LEFT
+            screenX > heroX -> RIGHT
+            else -> STILL
+        }
+    }
+
+    private fun between(compare: Int, min: Int, max: Int): Boolean {
+        return compare in (min + 1)..(max - 1)
     }
 }

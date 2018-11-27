a1a84fd8364f081f5ff7ce087bb27a8e6720e4f6
Gleb S <gleb.shalabotov@student.ucll.be>
2018-11-27 12:29:49 +0100

    Added gamestate class for managing the gamestate

--- core/src/be/ucll/dirkfalls/entities/EntityManager.kt
+++ core/src/be/ucll/dirkfalls/entities/EntityManager.kt
@@ -1,42 +1,55 @@
 package be.ucll.dirkfalls.entities
 
+import be.ucll.dirkfalls.GameConfig
+import be.ucll.dirkfalls.GameState
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer
+import com.badlogic.gdx.math.MathUtils
+import com.badlogic.gdx.math.Vector2
 
-/**
- * Handles all the things considering entities
- * Example: Spawning commets when required
- */
-interface EntityManager {
-    /**
-     * The hero entity
-     */
-    val hero: Hero
+class EntityManager(var gameState: GameState) {
 
     /**
-     * List containing all the comets
+     * Time since last comet spawned
      */
-    val comets: List<Comet>
+    private var cometTimer = 0f
 
     /**
-     * List containing all the entities (including the hero and commets)
+     * List of all rules used.
+     * The order is very important here!
      */
-    val entities: List<Entity>
+    private val rules = mutableListOf(
+        heroCannotMoveOutOfBounds,
+        updatePositionBasedOnVelocity,
+        heroTakesDamageWhenHit,
+        removeCometWhenOutOfBound
+    )
+
+    fun draw(renderer: ShapeRenderer) {
+        gameState.entities.forEach { it.drawDebug(renderer) }
+    }
+
+    fun update(delta: Float) {
+        rules.forEach { it(gameState, delta) }
+        createComet(delta)
+    }
+
+    private fun createComet(delta: Float) {
+        cometTimer += delta
+
+        if (cometTimer >= GameConfig.COMET_SPAWN_TIME) {
+            cometTimer = 0f // reset timer
+
+            val cometRadius = MathUtils.random(0.1f, 0.3f)
+            val cometX = MathUtils.random(0f + cometRadius, GameConfig.WORLD_WIDTH - cometRadius)
+            val vector2 = Vector2(cometX, GameConfig.WORLD_HEIGHT + cometRadius)
+            val comet = Comet(vector2, cometRadius)
+            if (checkCometSpawn(comet)) {
+                gameState.entities.add(comet)
+            }
+        }
+    }
 
 
-    /**
-     * Draw the entities
-     */
-    fun draw(renderer: ShapeRenderer)
 
-    /**
-     * Update the entities
-     */
-    fun update(delta: Float)
-
-    /**
-     * Remove an entity
-     * @return Was the entity deleted
-     */
-    fun deleteEntity(entity: Entity): Boolean
+    private fun checkCometSpawn(comet: Comet) = gameState.comets.none { it.overlaps(comet) }
 }
-

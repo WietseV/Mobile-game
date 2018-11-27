7cac75a04f0c56abf803635e45d114ad7d7ee1ea
Wietse Vandeput <wietse.vandeput@ucll.student.be>
2018-11-27 13:17:10 +0100

    homescreen

--- core/src/be/ucll/dirkfalls/DirkFallsGame.kt
+++ core/src/be/ucll/dirkfalls/DirkFallsGame.kt
@@ -1,6 +1,7 @@
 package be.ucll.dirkfalls
 
 import be.ucll.dirkfalls.screen.GameScreen
+import be.ucll.dirkfalls.screen.HomeScreen
 import com.badlogic.gdx.Application
 import com.badlogic.gdx.Game
 import com.badlogic.gdx.Gdx
@@ -8,7 +9,7 @@ import com.badlogic.gdx.Gdx
 class DirkFallsGame : Game() {
     override fun create() {
         Gdx.app.logLevel = Application.LOG_DEBUG
-        setScreen(GameScreen())
+        setScreen(HomeScreen())
     }
 
 
--- core/src/be/ucll/dirkfalls/screen/HomeScreen.kt
+++ core/src/be/ucll/dirkfalls/screen/HomeScreen.kt
@@ -1,33 +1,75 @@
 package be.ucll.dirkfalls.screen
 
+import be.ucll.dirkfalls.GameConfig
+import be.ucll.dirkfalls.utils.use
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.InputAdapter
 import com.badlogic.gdx.Screen
+import com.badlogic.gdx.graphics.GL20
+import com.badlogic.gdx.graphics.OrthographicCamera
+import com.badlogic.gdx.graphics.g2d.BitmapFont
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer
+import com.badlogic.gdx.utils.viewport.FitViewport
 
 class HomeScreen: Screen {
+
+    private val camera = OrthographicCamera()
+    private val performance = PerformanceLogger()
+    private val renderer = ShapeRenderer()
+    private var paused: Boolean = false
+
+    private val spriteBatch = SpriteBatch()
+    private val  font = BitmapFont()
+    private val str = "Hello World!";
+
+
+
+    private val viewport = FitViewport(GameConfig.WORLD_WIDTH, GameConfig.WORLD_HEIGHT, camera)
+
     override fun hide() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     override fun show() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        Gdx.input.inputProcessor = InputAdapter()
     }
 
     override fun render(delta: Float) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        performance.update(delta)
+
+        Gdx.gl.glClearColor(0f, 0f, 0f, 0f)
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
+
+        renderer.projectionMatrix = camera.combined
+        renderer.use {
+            renderer.setAutoShapeType(true)
+            renderer.set(ShapeRenderer.ShapeType.Filled)
+            renderer.setColor(0f, 0f, 255f, 100f)
+            renderer.rect(0f, 1f, GameConfig.WORLD_WIDTH, GameConfig.WORLD_HEIGHT)
+            renderer.setColor(0f, 255f, 0f, 100f)
+            renderer.rect(0f, 0f, GameConfig.WORLD_WIDTH, 1f)
+            renderer.setColor(255f, 255f, 255f, 100f)
+            renderer.setColor(255f, 255f, 255f, 100f)
+        }
+
+        spriteBatch.begin();
+        font.draw(spriteBatch, str, 10f, 10f);
+        spriteBatch.end();
     }
 
     override fun pause() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        paused = true
     }
 
     override fun resume() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        paused = false
     }
 
     override fun resize(width: Int, height: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        viewport.update(width, height, true)
     }
 
     override fun dispose() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        renderer.dispose()
     }
 }

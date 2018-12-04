9396e1ccdf3cdfe6bac133d12787e367ca3f9f49
JaroDeklerck <jaro.deklerck@student.kuleuven.be>
2018-12-04 12:27:04 +0100

    Game over screen home button

--- core/src/be/ucll/dirkfalls/screen/GameScreen.kt
+++ core/src/be/ucll/dirkfalls/screen/GameScreen.kt
@@ -6,9 +6,11 @@ import be.ucll.dirkfalls.GameConfig.WORLD_WIDTH
 import be.ucll.dirkfalls.GameState
 import be.ucll.dirkfalls.rules.LevelManager
 import be.ucll.dirkfalls.screen.buttons.ButtonTouchAdapter
+import be.ucll.dirkfalls.screen.buttons.HomeButton
 import be.ucll.dirkfalls.screen.buttons.ResetButton
 import be.ucll.dirkfalls.utils.rect
 import be.ucll.dirkfalls.entities.HeroDirection
+import be.ucll.dirkfalls.screen.buttons.Button
 import be.ucll.dirkfalls.utils.between
 import be.ucll.dirkfalls.utils.use
 import com.badlogic.gdx.Gdx
@@ -35,7 +37,8 @@ class GameScreen(val dirkFallsGame: DirkFallsGame) : DirkScreen() {
     private val performance = PerformanceLogger()
     private val ruleManager = LevelManager(gameState)
     private var paused: Boolean = false
-    private val reset = ResetButton(this)
+
+    private val buttons = mutableListOf<Button>()
 
 
     override fun hide() {
@@ -43,7 +46,12 @@ class GameScreen(val dirkFallsGame: DirkFallsGame) : DirkScreen() {
 
     override fun show() {
         Gdx.input.inputProcessor = GameTouchAdapter(gameState)
+        val reset = ResetButton(this)
+        val homeButton = HomeButton(this)
         reset.set(WORLD_WIDTH/2f-1f, WORLD_HEIGHT/2f-0.45f, 2f, 0.75f)
+        homeButton.set(WORLD_WIDTH/2f-1f, WORLD_HEIGHT/2f-1.5f, 2f, 0.75f)
+        buttons.add(reset)
+        buttons.add(homeButton)
         font.data.setScale(3f, 3f)
         font.region.texture.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear)
     }
@@ -132,37 +140,46 @@ class GameScreen(val dirkFallsGame: DirkFallsGame) : DirkScreen() {
 
     private fun drawGameOver() {
         renderer.use {
-            renderer.color = reset.color
             renderer.set(ShapeRenderer.ShapeType.Filled)
-            renderer.rect(reset.x,reset.y,reset.width,reset.height)
+            buttons.forEach {
+                renderer.color = it.color
+                renderer.rect(it)
+            }
         }
         batch.use {
             font.data.setScale(5f,5f)
             font.draw(batch, "Game over!", Gdx.graphics.width/3f-1f, 3f*Gdx.graphics.height/5f)
             font.data.setScale(3f, 3f)
             font.draw(batch, "Try again", Gdx.graphics.width/2f-10f, Gdx.graphics.height/2f, 20f, 1, false)
+            font.draw(batch, "Main menu", Gdx.graphics.width/2f-10f, Gdx.graphics.height/2.52f, 20f, 1, false)
         }
     }
 
     override fun touchUp(x: Float, y: Float) {
-        if (reset.contains(x, y)) {
-            reset.pressButton(dirkFallsGame)
+        buttons.forEach {
+            if (it.contains(x, y)) {
+                it.pressButton(dirkFallsGame, HomeScreen(dirkFallsGame))
+            }
         }
     }
 
     override fun touchDown(x: Float, y: Float) {
-        if (reset.contains(x, y)) {
-            reset.touchButton()
-        } else {
-            reset.touchMovedOff()
+        buttons.forEach {
+            if (it.contains(x, y)) {
+                it.touchButton()
+            } else {
+                it.touchMovedOff()
+            }
         }
     }
 
     override fun touchDragged(x: Float, y: Float) {
-        if (!reset.contains(x, y)) {
-            reset.touchMovedOff()
-        } else {
-            reset.touchButton()
+        buttons.forEach {
+            if (!it.contains(x, y)) {
+                it.touchMovedOff()
+            } else {
+                it.touchButton()
+            }
         }
     }
 

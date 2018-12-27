29379ea5411087743fa2da587a9cdf40efbab8b5
Robin Van den Broeck <robin.broeck@gmail.com>
2018-12-27 16:19:52 +0100

    Fixed table

--- core/src/be/ucll/dirkfalls/HighscoretableScreen.kt
+++ core/src/be/ucll/dirkfalls/HighscoretableScreen.kt
@@ -7,93 +7,68 @@ import be.ucll.dirkfalls.utils.logger
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.Screen
 import com.badlogic.gdx.graphics.GL20
-import com.badlogic.gdx.scenes.scene2d.InputEvent
-import com.badlogic.gdx.scenes.scene2d.InputListener
 import com.badlogic.gdx.scenes.scene2d.Stage
-import com.badlogic.gdx.scenes.scene2d.ui.*
+import com.badlogic.gdx.scenes.scene2d.ui.Label
+import com.badlogic.gdx.scenes.scene2d.ui.Skin
+import com.badlogic.gdx.scenes.scene2d.ui.Table
+import com.badlogic.gdx.scenes.scene2d.ui.Value
 
 
-class HighscoretableScreen :
-            Screen {
-        var stage: Stage = Stage()
+class HighscoretableScreen : Screen {
+    var stage: Stage = Stage()
     override fun hide() {
-        }
+    }
 
-        fun getTop10(): ArrayList<HighscoreEntry> {
-            var counter = 0
-            val list = ArrayList<HighscoreEntry>(10)
-            val highscoreService = HighscoreService()
-            highscoreService.all(object : AsyncHandler<List<HighscoreEntry>> {
-                override fun success(data: List<HighscoreEntry>) {
-                    val logger = logger<DirkFallsGame>()
-                    for(entry in data) {
-                        while (counter < 10) {
-                            logger.info(entry.name + " -- " + entry.score)
-                            list.add(entry)
-                            logger<DirkFallsGame>().info(list.size.toString())
-                            counter += 1
-                        }
-                    }
+    override fun show() {
+        stage = Stage()
+        Gdx.input.inputProcessor = stage
+        val skin = Skin(Gdx.files.internal("UI/skin.json"))
 
+        val table = Table()
+        table.setSize(Gdx.graphics.width.toFloat(), Gdx.graphics.height.toFloat())
+        table.setPosition(0f, 0f)
+        stage.addActor(table)
+
+        table.defaults().width(Value.percentWidth(2f))
+
+        val highscoreService = HighscoreService()
+        highscoreService.all(object : AsyncHandler<List<HighscoreEntry>> {
+            override fun success(data: List<HighscoreEntry>) {
+                for (entry in data) {
+                    val itemLabel = Label(entry.name, skin)
+                    table.add(itemLabel)
+                    val scoreLabel = Label(entry.score.toString(), skin)
+                    table.add(scoreLabel)
+                    table.row()
                 }
-                override fun error(t: Throwable) {
-                    logger<DirkFallsGame>().error(t.toString(), t)
-                }
-            })
-            return list
-        }
-
-        override fun show() {
-            val list =getTop10()
-
-            stage = Stage()
-            Gdx.input.inputProcessor = stage
-            val skin = Skin(Gdx.files.internal("UI/skin.json"))
-
-
-            val table = Table()
-            table.setSize(Gdx.graphics.width.toFloat(), Gdx.graphics.height.toFloat())
-            table.setPosition(0f, 0f)
-            stage.addActor(table)
-
-            table.debug()
-
-
-            table.defaults().width(Value.percentWidth(2f))
-            val nameLabel = Label("Name", skin)
-            table.add(nameLabel)
-            val scoreLabel = Label("Score", skin)
-            table.add(scoreLabel)
-            table.row()
-            logger<DirkFallsGame>().info(list.size.toString())
-            for (entry in list){
-                val itemLabel = Label(entry.name, skin)
-                table.add(itemLabel)
-                val scoreLabel = Label("10", skin)
-                table.add(scoreLabel)
-                table.row()
             }
-        }
 
-        override fun render(delta: Float) {
-            Gdx.gl.glClearColor(0.2f, 0.2f, 0.2f, 1f)
-            Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
-            //stage.act(Math.min(Gdx.graphics.deltaTime, 1 / 30f))
-            stage.draw()
-        }
-
-        override fun pause() {
-        }
-
-        override fun resume() {
-        }
-
-        override fun resize(width: Int, height: Int) {
-            stage.viewport.update(width, height, true)
-        }
-
-        override fun dispose() {
-            stage.dispose()
-        }
+            override fun error(t: Throwable) {
+                logger<DirkFallsGame>().error(t.toString(), t)
+            }
+        })
 
     }
+
+    override fun render(delta: Float) {
+        Gdx.gl.glClearColor(0.2f, 0.2f, 0.2f, 1f)
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
+        stage.act(Math.min(Gdx.graphics.deltaTime, 1 / 30f))
+        stage.draw()
+    }
+
+    override fun pause() {
+    }
+
+    override fun resume() {
+    }
+
+    override fun resize(width: Int, height: Int) {
+        stage.viewport.update(width, height, true)
+    }
+
+    override fun dispose() {
+        stage.dispose()
+    }
+
+}
